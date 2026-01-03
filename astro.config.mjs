58cba30f188da0b465397422e656106b6c31c3fd
SwiftDust <mart.zielman@kpnmail.nl>
2026-01-03 22:43:41 +0100

    Add Node.js SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,4 +13,8 @@ export default defineConfig({
   },
 
   integrations: [icon()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
